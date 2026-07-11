2cadb1af93bc49aaaf2da4caf63893962a5eedcc
Rounak Prasad <rounak16112006@gmail.com>
2026-07-12 00:15:37 +0530

    feat add task manager

---
+++ Tutorials/Week 3/Task Management Application/main.js
@@ -0,0 +1,31 @@
+const app = new Vue({
+    el : "#app", 
+    data : {
+        mytasks : [],
+        task : ""
+    },
+    methods : {
+        add_task : function(){
+            let task = {}
+            task.id = this.mytasks.length + 1
+            task.name = this.task
+            task.isCompleted = false
+            this.mytasks.push(task)
+            this.task = ""
+        }, 
+        mark_complete : function(id){
+            let thistask = this.mytasks.find((task) => task.id == id)
+            thistask.isCompleted = true
+        }
+    },
+    computed : {
+        completedTasks : function(){
+            let tasks = this.mytasks.filter((task) => task.isCompleted == true)
+            return tasks.length
+        }, 
+        pendingTasks : function(){
+            // let tasks = this.mytasks.filter((task) => task.isCompleted == true)
+            return this.mytasks.length - this.completedTasks 
+        }
+    }
+})
