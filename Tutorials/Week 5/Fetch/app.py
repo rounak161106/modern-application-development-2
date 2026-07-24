eb8b888462234aa1cc7d305e5011e54dea098f96
Rounak Prasad <rounak16112006@gmail.com>
2026-07-24 22:16:54 +0530

    complete basic get request

--- Tutorials/Week 5/Fetch/app.py
+++ Tutorials/Week 5/Fetch/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +7,10 @@ CORS(app)
 @app.route("/")
 def home():
     return {'message' : 'Hello World'}
+
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify(error="Page not found"), 404
 
 if __name__ == "__main__":
     app.run(debug = True)
