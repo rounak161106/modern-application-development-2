5f2fd6fb32c124906a4b31847c76860aa8cd5f0d
Rounak Prasad <rounak16112006@gmail.com>
2026-09-02 22:05:15 +0530

    update

---
+++ Tutorials/Week 7/vue_cli/babel.config.js
@@ -0,0 +1,7 @@
+// used as a configuration file for Babel in a Vue.js project created with Vue CLI. It specifies the presets that Babel should use to transpile the JavaScript code. In this case, it uses the '@vue/cli-plugin-babel/preset' preset, which includes the necessary plugins and configurations for a Vue.js application.
+
+module.exports = {
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
